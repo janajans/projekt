847b5e7a35f460d910c9a5c91696294eca047b35
Viktorie Melichová <viktoriemelichova@gmail.com>
2021-06-05 10:47:17 +0200

    source array of letters and images

---
+++ src/letterObject.js
@@ -0,0 +1,299 @@
+const letterObjects = [
+  {
+    name: 'letterA',
+    letter: {
+      lowercase: './assets/letters/a-lowercase.svg',
+      uppercase: './assets/letters/a-uppercase.svg',
+      sound: '',
+    },
+    picture: { image: './assets/images/autobus.svg', sound: '' },
+  },
+  {
+    name: 'letterB',
+    letter: {
+      lowercase: './assets/letters/b-lowercase.svg',
+      uppercase: './assets/letters/b-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/buben.svg', sound: '' },
+  },
+  {
+    name: 'letterC',
+    letter: {
+      lowercase: './assets/letters/c-lowercase.svg',
+      uppercase: './assets/letters/c-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/cirkus.svg', sound: '' },
+  },
+  {
+    name: 'letterCC',
+    letter: {
+      lowercase: './assets/letters/cc-lowercase.svg',
+      uppercase: './assets/letters/cc-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/cepice.svg', sound: '' },
+  },
+  {
+    name: 'letterD',
+    letter: {
+      lowercase: './assets/letters/d-lowercase.svg',
+      uppercase: './assets/letters/d-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/dort.svg', sound: '' },
+  },
+  {
+    name: 'letterDD',
+    letter: {
+      lowercase: './assets/letters/dd-lowercase.svg',
+      uppercase: './assets/letters/dd-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/dabel.svg', sound: '' },
+  },
+  {
+    name: 'letterE',
+    letter: {
+      lowercase: './assets/letters/e-lowercase.svg',
+      uppercase: './assets/letters/e-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/erb.svg', sound: '' },
+  },
+  {
+    name: 'letterF',
+    letter: {
+      lowercase: './assets/letters/f-lowercase.svg',
+      uppercase: './assets/letters/f-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/fletna.svg', sound: '' },
+  },
+  {
+    name: 'letterG',
+    letter: {
+      lowercase: './assets/letters/g-lowercase.svg',
+      uppercase: './assets/letters/g-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/gorila.svg', sound: '' },
+  },
+  {
+    name: 'letterH',
+    letter: {
+      lowercase: './assets/letters/h-lowercase.svg',
+      uppercase: './assets/letters/h-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/hodiny.svg', sound: '' },
+  },
+  {
+    name: 'letterCH',
+    letter: {
+      lowercase: './assets/letters/ch-lowercase.svg',
+      uppercase: './assets/letters/ch-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/chleba.svg', sound: '' },
+  },
+  {
+    name: 'letterI',
+    letter: {
+      lowercase: './assets/letters/i-lowercase.svg',
+      uppercase: './assets/letters/i-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/iglu.svg', sound: '' },
+  },
+  {
+    name: 'letterJ',
+    letter: {
+      lowercase: './assets/letters/j-lowercase.svg',
+      uppercase: './assets/letters/j-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/jablko.svg', sound: '' },
+  },
+  {
+    name: 'letterK',
+    letter: {
+      lowercase: './assets/letters/k-lowercase.svg',
+      uppercase: './assets/letters/k-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/kasparek.svg', sound: '' },
+  },
+  {
+    name: 'letterL',
+    letter: {
+      lowercase: './assets/letters/l-lowercase.svg',
+      uppercase: './assets/letters/l-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/lodicka.svg', sound: '' },
+  },
+  {
+    name: 'letterM',
+    letter: {
+      lowercase: './assets/letters/m-lowercase.svg',
+      uppercase: './assets/letters/m-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/motyl.svg', sound: '' },
+  },
+  {
+    name: 'letterN',
+    letter: {
+      lowercase: './assets/letters/n-lowercase.svg',
+      uppercase: './assets/letters/n-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/noty.svg', sound: '' },
+  },
+  {
+    name: 'letterNN',
+    letter: {
+      lowercase: './assets/letters/nn-lowercase.svg',
+      uppercase: './assets/letters/nn-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/nuf.svg', sound: '' },
+  },
+  {
+    name: 'letterO',
+    letter: {
+      lowercase: './assets/letters/o-lowercase.svg',
+      uppercase: './assets/letters/o-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/oko.svg', sound: '' },
+  },
+  {
+    name: 'letterP',
+    letter: {
+      lowercase: './assets/letters/p-lowercase.svg',
+      uppercase: './assets/letters/p-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/pero.svg', sound: '' },
+  },
+  {
+    name: 'letterR',
+    letter: {
+      lowercase: './assets/letters/r-lowercase.svg',
+      uppercase: './assets/letters/r-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/raketa.svg', sound: '' },
+  },
+  {
+    name: 'letterRR',
+    letter: {
+      lowercase: './assets/letters/rr-lowercase.svg',
+      uppercase: './assets/letters/rr-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/redkvicka.svg', sound: '' },
+  },
+  {
+    name: 'letterS',
+    letter: {
+      lowercase: './assets/letters/s-lowercase.svg',
+      uppercase: './assets/letters/s-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/slon.svg', sound: '' },
+  },
+  {
+    name: 'letterSS',
+    letter: {
+      lowercase: './assets/letters/ss-lowercase.svg',
+      uppercase: './assets/letters/ss-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/sala.svg', sound: '' },
+  },
+  {
+    name: 'letterT',
+    letter: {
+      lowercase: './assets/letters/t-lowercase.svg',
+      uppercase: './assets/letters/t-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/tulipan.svg', sound: '' },
+  },
+  {
+    name: 'letterTT',
+    letter: {
+      lowercase: './assets/letters/tt-lowercase.svg',
+      uppercase: './assets/letters/tt-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/tuhyk.svg', sound: '' },
+  },
+  {
+    name: 'letterU',
+    letter: {
+      lowercase: './assets/letters/u-lowercase.svg',
+      uppercase: './assets/letters/u-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/upir.svg', sound: '' },
+  },
+  {
+    name: 'letterV',
+    letter: {
+      lowercase: './assets/letters/v-lowercase.svg',
+      uppercase: './assets/letters/v-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/veverka.svg', sound: '' },
+  },
+  {
+    name: 'letterW',
+    letter: {
+      lowercase: './assets/letters/w-lowercase.svg',
+      uppercase: './assets/letters/w-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/wendy.svg', sound: '' },
+  },
+  {
+    name: 'letterX',
+    letter: {
+      lowercase: './assets/letters/x-lowercase.svg',
+      uppercase: './assets/letters/x-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/xylofon.svg', sound: '' },
+  },
+  {
+    name: 'letterY',
+    letter: {
+      lowercase: './assets/letters/y-lowercase.svg',
+      uppercase: './assets/letters/y-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/yetti.svg', sound: '' },
+  },
+  {
+    name: 'letterZ',
+    letter: {
+      lowercase: './assets/letters/z-lowercase.svg',
+      uppercase: './assets/letters/z-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/zmrzlina.svg', sound: '' },
+  },
+  {
+    name: 'letterZZ',
+    letter: {
+      lowercase: './assets/letters/zz-lowercase.svg',
+      uppercase: './assets/letters/zz-uppercase.svg',
+      sound,
+    },
+    picture: { image: './assets/images/zalud.svg', sound: '' },
+  },
+];
