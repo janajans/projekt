9b61362c4cd0d588dcc9a69bebab6b945bf20fbe
janajans <1janajansova@gmail.com>
2021-06-05 18:18:26 +0200

    some deleted images

--- src/Components/Pexeso/index.jsx
+++ src/Components/Pexeso/index.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const Pexeso = () => <p>pexeso page</p>;
+const Pexeso = () => {
+  const [randomIndex, setRandomIndex] = useState(
+    Math.floor(Math.random() * letterObjects.length),
+  );
+
+  return (
+    <>
+      <div className="container-pexeso">
+        <img
+          className="pexeso-image"
+          src={letterObjects[randomIndex].picture.image}
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
 
 export default Pexeso;
