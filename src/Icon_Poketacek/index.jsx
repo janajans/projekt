f12c5615eb6b412df841e8e06aa060da5429d762
janajans <1janajansova@gmail.com>
2021-06-05 10:52:57 +0200

    sounds test

--- src/Icon_Poketacek/index.jsx
+++ src/Icon_Poketacek/index.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 import './style.css';
 import icon_poketacek from './img/poketacek-prozatimni.png';
 
-const Icon_Poketacek = () => (
-  <img id="icon_poketacek" src={icon_poketacek} alt="mascot Poketacek" />
-);
+const Icon_Poketacek = () => {
+  let audio = new Audio('../assets/sounds/success.wav');
+
+  const start = () => {
+    audio.play();
+  };
+
+  return (
+    <img
+      id="icon_poketacek"
+      src={icon_poketacek}
+      alt="mascot Poketacek"
+      onClick={start}
+    />
+  );
+};
 
 export default Icon_Poketacek;
--- src/Learn/index.jsx
+++ src/Learn/index.jsx
@@ -3,14 +3,21 @@ import './style.css';
 import bus from './img/autobus.png';
 import Header_long from '../Header_long';
 
-const Learn = () => (
-  <>
-    <Header_long />
-    <div className="container--learn">
-      <img id="bus" src={bus} alt="picture of a bus" />
-    </div>
-  </>
-);
+const Learn = () => {
+  let audio = new Audio('../assets/sounds/failure.wav');
+
+  const start = () => {
+    audio.play();
+  };
+  return (
+    <>
+      <Header_long />
+      <div className="container--learn">
+        <img id="bus" src={bus} alt="picture of a bus" onClick={start} />
+      </div>
+    </>
+  );
+};
 
 export default Learn;
 
