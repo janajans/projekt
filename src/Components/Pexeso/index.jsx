43674513b1524f52afd3f579193feea1124c3289
janajans <1janajansova@gmail.com>
2021-06-06 10:44:07 +0200

    pexeso selected letter

--- src/Components/Pexeso/index.jsx
+++ src/Components/Pexeso/index.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import letterObjects from '../../letterObjects';
 import { randomIndexesGenerator } from '../../Util/randomIndexGenerator';
 import './style.css';
-import { shuffledIndexes } from '../../Util/arrayShuffler';
+import { shuffledIndexes as shuffle } from '../../Util/arrayShuffler';
 
 const Pexeso = () => {
   const [randomIndexes] = useState(randomIndexesGenerator(3));
+  const [selectedLetter, setSelectedLetter] = useState(null);
+  const [shuffledIndexes, setSchufledIndexes] = useState([]);
 
+  useEffect(() => {
+    setSchufledIndexes(shuffle(randomIndexes));
+  }, []);
   return (
     <>
       <div className="container-pexeso">
@@ -21,14 +26,22 @@ const Pexeso = () => {
           ))}
         </div>
         <div className="pexeso-letters">
-          {shuffledIndexes(randomIndexes).map((index) => (
-            <img
-              className="pexeso-item"
-              src={letterObjects[index].letter.uppercase}
-              alt=""
-              key={index}
-            />
-          ))}
+          {shuffledIndexes.map((index) => {
+            const selected = index === selectedLetter;
+            return (
+              <img
+                className={
+                  selected ? 'pexeso-item pexeso-item-selected' : 'pexeso-item'
+                }
+                src={letterObjects[index].letter.uppercase}
+                alt=""
+                key={index}
+                onClick={() => {
+                  setSelectedLetter(index);
+                }}
+              />
+            );
+          })}
         </div>
       </div>
     </>
