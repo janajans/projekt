9375c2c01e7adb188fc4d944045d731c59f8084e
Viktorie Melichová <viktoriemelichova@gmail.com>
2021-06-06 14:23:38 +0200

    drag and drop is working

--- src/Components/DragAndDrop/Target.jsx
+++ src/Components/DragAndDrop/Target.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDrop } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
 import letterObjects from '../../letterObjects';
 
+const options = {
+  default: 'default',
+  success: 'success',
+  unsuccess: 'unsuccess',
+};
+
 export const Target = ({ index }) => {
+  const [successful, setSuccess] = useState(options.default);
+
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
     accept: ItemTypes.OPTION,
     drop: (item) => {
       console.log(item);
-    },
-    /*  collect: (monitor) => ({ 
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }), */
-  }));
 
-  /*   const isActive = canDrop && isOver;
-   */
+      if (index !== item.index) {
+        setSuccess(options.unsuccess);
+      } else if (index === item.index) {
+        setSuccess(options.success);
+      }
+      setTimeout(() => setSuccess(options.default), 1000);
+    },
+  }));
+  let classname = '';
+
+  if (successful === options.success) {
+    classname = 'target--successful';
+  } else if (successful === options.unsuccess) {
+    classname = 'target--unsuccessful';
+  }
+
+  console.log(classname);
+
   return (
-    <div ref={drop} role={'Dustbin'} className="target">
-      {/*       {isActive ? 'Release to drop' : 'Drag a picture here'}
-       */}
-      <img src={letterObjects[index].picture.image} alt="" />
+    <div ref={drop} className="target">
+      <img
+        src={letterObjects[index].picture.image}
+        className={classname}
+        alt=""
+      />
     </div>
   );
 };
