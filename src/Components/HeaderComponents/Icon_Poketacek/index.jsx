708fa58989a40e423abd46cce0f04f40ec455002
janajans <1janajansova@gmail.com>
2021-06-07 15:37:34 +0200

    sounds added, letterObjects updated

--- src/Components/HeaderComponents/Icon_Poketacek/index.jsx
+++ src/Components/HeaderComponents/Icon_Poketacek/index.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import './style.css';
 import icon_poketacek from './img/poketacek-prozatimni.png';
 
-const Icon_Poketacek = () => (
-  <img id="icon_poketacek" src={icon_poketacek} alt="mascot Poketacek" />
-);
+const Icon_Poketacek = () => {
+  let audioAhojVyver = new Audio('../../assets/sounds/ahojVyber.mp3');
+  return (
+    <img
+      id="icon_poketacek"
+      src={icon_poketacek}
+      alt="mascot Poketacek"
+      onClick={() => {
+        audioAhojVyver.play();
+      }}
+    />
+  );
+};
 
 export default Icon_Poketacek;
--- src/letterObjects.js
+++ src/letterObjects.js
@@ -4,297 +4,396 @@ const letterObjects = [
     letter: {
       lowercase: './assets/letters/a-lowercase.svg',
       uppercase: './assets/letters/a-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/a.mp3',
     },
-    picture: { image: './assets/images/autobus.svg', sound: '' },
+    picture: {
+      image: './assets/images/autobus.svg',
+      sound: './assets/sounds/soundsOfPictures/autobus.mp3',
+    },
   },
   {
     name: 'letterB',
     letter: {
       lowercase: './assets/letters/b-lowercase.svg',
       uppercase: './assets/letters/b-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/b.mp3',
     },
-    picture: { image: './assets/images/buben.svg', sound: '' },
+    picture: {
+      image: './assets/images/buben.svg',
+      sound: './assets/sounds/soundsOfPictures/buben.mp3',
+    },
   },
   {
     name: 'letterC',
     letter: {
       lowercase: './assets/letters/c-lowercase.svg',
       uppercase: './assets/letters/c-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/c.mp3',
     },
-    picture: { image: './assets/images/cirkus.svg', sound: '' },
+    picture: {
+      image: './assets/images/cirkus.svg',
+      sound: './assets/sounds/soundsOfPictures/cirkus.mp3',
+    },
   },
   {
     name: 'letterCC',
     letter: {
       lowercase: './assets/letters/cc-lowercase.svg',
       uppercase: './assets/letters/cc-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/cc.mp3',
     },
-    picture: { image: './assets/images/cepice.svg', sound: '' },
+    picture: {
+      image: './assets/images/cepice.svg',
+      sound: './assets/sounds/soundsOfPictures/cepice.mp3',
+    },
   },
   {
     name: 'letterD',
     letter: {
       lowercase: './assets/letters/d-lowercase.svg',
       uppercase: './assets/letters/d-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/d.mp3',
     },
-    picture: { image: './assets/images/dort.svg', sound: '' },
+    picture: {
+      image: './assets/images/dort.svg',
+      sound: './assets/sounds/soundsOfPictures/dort.mp3',
+    },
   },
   {
     name: 'letterDD',
     letter: {
       lowercase: './assets/letters/dd-lowercase.svg',
       uppercase: './assets/letters/dd-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/dd.mp3',
     },
-    picture: { image: './assets/images/dabel.svg', sound: '' },
+    picture: {
+      image: './assets/images/dabel.svg',
+      sound: './assets/sounds/soundsOfPictures/dabel.mp3',
+    },
   },
   {
     name: 'letterE',
     letter: {
       lowercase: './assets/letters/e-lowercase.svg',
       uppercase: './assets/letters/e-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/e.mp3',
     },
-    picture: { image: './assets/images/erb.svg', sound: '' },
+    picture: {
+      image: './assets/images/erb.svg',
+      sound: './assets/sounds/soundsOfPictures/erb.mp3',
+    },
   },
   {
     name: 'letterF',
     letter: {
       lowercase: './assets/letters/f-lowercase.svg',
       uppercase: './assets/letters/f-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/f.mp3',
     },
-    picture: { image: './assets/images/fletna.svg', sound: '' },
+    picture: {
+      image: './assets/images/fletna.svg',
+      sound: './assets/sounds/soundsOfPictures/fletna.mp3',
+    },
   },
   {
     name: 'letterG',
     letter: {
       lowercase: './assets/letters/g-lowercase.svg',
       uppercase: './assets/letters/g-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/g.mp3',
     },
-    picture: { image: './assets/images/gorila.svg', sound: '' },
+    picture: {
+      image: './assets/images/gorila.svg',
+      sound: './assets/sounds/soundsOfPictures/gorila.mp3',
+    },
   },
   {
     name: 'letterH',
     letter: {
       lowercase: './assets/letters/h-lowercase.svg',
       uppercase: './assets/letters/h-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/h.mp3',
     },
-    picture: { image: './assets/images/hodiny.svg', sound: '' },
+    picture: {
+      image: './assets/images/hodiny.svg',
+      sound: './assets/sounds/soundsOfPictures/hodiny.mp3',
+    },
   },
   {
     name: 'letterCH',
     letter: {
       lowercase: './assets/letters/ch-lowercase.svg',
       uppercase: './assets/letters/ch-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/ch.mp3',
     },
-    picture: { image: './assets/images/chleba.svg', sound: '' },
+    picture: {
+      image: './assets/images/chleba.svg',
+      sound: './assets/sounds/soundsOfPictures/chleba.mp3',
+    },
   },
   {
     name: 'letterI',
     letter: {
       lowercase: './assets/letters/i-lowercase.svg',
       uppercase: './assets/letters/i-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/i.mp3',
     },
-    picture: { image: './assets/images/iglu.svg', sound: '' },
+    picture: {
+      image: './assets/images/iglu.svg',
+      sound: './assets/sounds/soundsOfPictures/iglu.mp3',
+    },
   },
   {
     name: 'letterJ',
     letter: {
       lowercase: './assets/letters/j-lowercase.svg',
       uppercase: './assets/letters/j-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/j.mp3',
     },
-    picture: { image: './assets/images/jablko.svg', sound: '' },
+    picture: {
+      image: './assets/images/jablko.svg',
+      sound: './assets/sounds/soundsOfPictures/jablko.mp3',
+    },
   },
   {
     name: 'letterK',
     letter: {
       lowercase: './assets/letters/k-lowercase.svg',
       uppercase: './assets/letters/k-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/k.mp3',
     },
-    picture: { image: './assets/images/kasparek.svg', sound: '' },
+    picture: {
+      image: './assets/images/kasparek.svg',
+      sound: './assets/sounds/soundsOfPictures/klaun.mp3',
+    },
   },
   {
     name: 'letterL',
     letter: {
       lowercase: './assets/letters/l-lowercase.svg',
       uppercase: './assets/letters/l-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/l.mp3',
     },
-    picture: { image: './assets/images/lodicka.svg', sound: '' },
+    picture: {
+      image: './assets/images/lodicka.svg',
+      sound: './assets/sounds/soundsOfPictures/lod.mp3',
+    },
   },
   {
     name: 'letterM',
     letter: {
       lowercase: './assets/letters/m-lowercase.svg',
       uppercase: './assets/letters/m-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/m.mp3',
     },
-    picture: { image: './assets/images/motyl.svg', sound: '' },
+    picture: {
+      image: './assets/images/motyl.svg',
+      sound: './assets/sounds/soundsOfPictures/motyl.mp3',
+    },
   },
   {
     name: 'letterN',
     letter: {
       lowercase: './assets/letters/n-lowercase.svg',
       uppercase: './assets/letters/n-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/n.mp3',
     },
-    picture: { image: './assets/images/noty.svg', sound: '' },
+    picture: {
+      image: './assets/images/noty.svg',
+      sound: './assets/sounds/soundsOfPictures/nota.mp3',
+    },
   },
   {
     name: 'letterNN',
     letter: {
       lowercase: './assets/letters/nn-lowercase.svg',
       uppercase: './assets/letters/nn-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/nn.mp3',
     },
-    picture: { image: './assets/images/nuf.svg', sound: '' },
+    picture: {
+      image: './assets/images/nuf.svg',
+      sound: './assets/sounds/soundsOfPictures/nnuf.mp3',
+    },
   },
   {
     name: 'letterO',
     letter: {
       lowercase: './assets/letters/o-lowercase.svg',
       uppercase: './assets/letters/o-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/o.mp3',
     },
-    picture: { image: './assets/images/oko.svg', sound: '' },
+    picture: {
+      image: './assets/images/oko.svg',
+      sound: './assets/sounds/soundsOfPictures/oko.mp3',
+    },
   },
   {
     name: 'letterP',
     letter: {
       lowercase: './assets/letters/p-lowercase.svg',
       uppercase: './assets/letters/p-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/p.mp3',
     },
-    picture: { image: './assets/images/pero.svg', sound: '' },
+    picture: {
+      image: './assets/images/pero.svg',
+      sound: './assets/sounds/soundsOfPictures/pirko.mp3',
+    },
   },
   {
     name: 'letterR',
     letter: {
       lowercase: './assets/letters/r-lowercase.svg',
       uppercase: './assets/letters/r-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/r.mp3',
     },
-    picture: { image: './assets/images/raketa.svg', sound: '' },
+    picture: {
+      image: './assets/images/raketa.svg',
+      sound: './assets/sounds/soundsOfPictures/raketa.mp3',
+    },
   },
   {
     name: 'letterRR',
     letter: {
       lowercase: './assets/letters/rr-lowercase.svg',
       uppercase: './assets/letters/rr-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/rr.mp3',
     },
-    picture: { image: './assets/images/redkvicka.svg', sound: '' },
+    picture: {
+      image: './assets/images/redkvicka.svg',
+      sound: './assets/sounds/soundsOfPictures/redkvicka.mp3',
+    },
   },
   {
     name: 'letterS',
     letter: {
       lowercase: './assets/letters/s-lowercase.svg',
       uppercase: './assets/letters/s-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/s.mp3',
     },
-    picture: { image: './assets/images/slon.svg', sound: '' },
+    picture: {
+      image: './assets/images/slon.svg',
+      sound: './assets/sounds/soundsOfPictures/slon.mp3',
+    },
   },
   {
     name: 'letterSS',
     letter: {
       lowercase: './assets/letters/ss-lowercase.svg',
       uppercase: './assets/letters/ss-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/ss.mp3',
     },
-    picture: { image: './assets/images/sala.svg', sound: '' },
+    picture: {
+      image: './assets/images/sala.svg',
+      sound: './assets/sounds/soundsOfPictures/sala.mp3',
+    },
   },
   {
     name: 'letterT',
     letter: {
       lowercase: './assets/letters/t-lowercase.svg',
       uppercase: './assets/letters/t-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/t.mp3',
     },
-    picture: { image: './assets/images/tulipan.svg', sound: '' },
+    picture: {
+      image: './assets/images/tulipan.svg',
+      sound: './assets/sounds/soundsOfPictures/tulipan.mp3',
+    },
   },
   {
     name: 'letterTT',
     letter: {
       lowercase: './assets/letters/tt-lowercase.svg',
       uppercase: './assets/letters/tt-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/tt.mp3',
     },
-    picture: { image: './assets/images/tuhyk.svg', sound: '' },
+    picture: {
+      image: './assets/images/tuhyk.svg',
+      sound: './assets/sounds/soundsOfPictures/tuhyk.mp3',
+    },
   },
   {
     name: 'letterU',
     letter: {
       lowercase: './assets/letters/u-lowercase.svg',
       uppercase: './assets/letters/u-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/u.mp3',
     },
-    picture: { image: './assets/images/upir.svg', sound: '' },
+    picture: {
+      image: './assets/images/upir.svg',
+      sound: './assets/sounds/soundsOfPictures/upir.mp3',
+    },
   },
   {
     name: 'letterV',
     letter: {
       lowercase: './assets/letters/v-lowercase.svg',
       uppercase: './assets/letters/v-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/v.mp3',
     },
-    picture: { image: './assets/images/veverka.svg', sound: '' },
+    picture: {
+      image: './assets/images/veverka.svg',
+      sound: './assets/sounds/soundsOfPictures/veverka.mp3',
+    },
   },
   {
     name: 'letterW',
     letter: {
       lowercase: './assets/letters/w-lowercase.svg',
       uppercase: './assets/letters/w-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/w.mp3',
     },
-    picture: { image: './assets/images/wendy.svg', sound: '' },
+    picture: {
+      image: './assets/images/wendy.svg',
+      sound: './assets/sounds/soundsOfPictures/wendy.mp3',
+    },
   },
   {
     name: 'letterX',
     letter: {
       lowercase: './assets/letters/x-lowercase.svg',
       uppercase: './assets/letters/x-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/x.mp3',
     },
-    picture: { image: './assets/images/xylofon.svg', sound: '' },
+    picture: {
+      image: './assets/images/xylofon.svg',
+      sound: './assets/sounds/soundsOfPictures/xylofon.mp3',
+    },
   },
   {
     name: 'letterY',
     letter: {
       lowercase: './assets/letters/y-lowercase.svg',
       uppercase: './assets/letters/y-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/y.mp3',
     },
-    picture: { image: './assets/images/yetti.svg', sound: '' },
+    picture: {
+      image: './assets/images/yetti.svg',
+      sound: './assets/sounds/soundsOfPictures/yeti.mp3',
+    },
   },
   {
     name: 'letterZ',
     letter: {
       lowercase: './assets/letters/z-lowercase.svg',
       uppercase: './assets/letters/z-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/z.mp3',
     },
-    picture: { image: './assets/images/zmrzlina.svg', sound: '' },
+    picture: {
+      image: './assets/images/zmrzlina.svg',
+      sound: './assets/sounds/soundsOfPictures/zmrzlina.mp3',
+    },
   },
   {
     name: 'letterZZ',
     letter: {
       lowercase: './assets/letters/zz-lowercase.svg',
       uppercase: './assets/letters/zz-uppercase.svg',
-      sound: '',
+      sound: './assets/sounds/soundsOfLetters/zz.mp3',
     },
-    picture: { image: './assets/images/zalud.svg', sound: '' },
+    picture: {
+      image: './assets/images/zalud.svg',
+      sound: './assets/sounds/soundsOfPictures/zalud.mp3',
+    },
   },
 ];
 
