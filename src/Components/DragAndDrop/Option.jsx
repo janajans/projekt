0b58fd952ce5da30166ffc55bd3612227d5232eb
janajans <1janajansova@gmail.com>
2021-06-09 22:20:52 +0200

    sound of letters on dnd page working

--- src/Components/DragAndDrop/Option.jsx
+++ src/Components/DragAndDrop/Option.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { SoundContext } from '../../Providers/SoundContext';
 import { useDrag } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
 import letterObjects from '../../letterObjects';
 import { CustomDragLayer } from '../DragAndDrop/CustomDragLayer';
 
 export const Option = function Option({ index }) {
+  const { soundOn } = useContext(SoundContext);
   const [{ isDragging }, drag] = useDrag(() => ({
     type: ItemTypes.OPTION,
     item: { index },
@@ -15,19 +17,32 @@ export const Option = function Option({ index }) {
     collect: (monitor) => ({ isDragging: monitor.isDragging() }),
   }));
 
+  let audioLetters = new Audio(letterObjects[index].letter.sound);
+
   return (
     <>
       {isDragging ? (
         <CustomDragLayer
           item={
             <div className="option">
-              <img src={letterObjects[index].letter.uppercase} alt="" />
+              <img
+                src={letterObjects[index].letter.uppercase}
+                alt="letter in drag and drop game"
+              />
             </div>
           }
         />
       ) : (
         <div ref={drag} role="Option" className="option">
-          <img src={letterObjects[index].letter.uppercase} alt="" />
+          <img
+            src={letterObjects[index].letter.uppercase}
+            alt="letter in drag and drop game"
+            onClick={() => {
+              if (soundOn) {
+                audioLetters.play();
+              }
+            }}
+          />
         </div>
       )}
     </>
