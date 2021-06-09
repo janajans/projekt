ab27bd68cc88443786b41ecae2cca43282a2a3a9
janajans <1janajansova@gmail.com>
2021-06-09 15:10:55 +0200

    no idea what I am doing

--- src/Components/HeaderComponents/Icon_Poketacek/index.jsx
+++ src/Components/HeaderComponents/Icon_Poketacek/index.jsx
@@ -2,20 +2,57 @@ import React, { useContext } from 'react';
 import './style.css';
 import icon_poketacek from './img/poketacek-prozatimni.png';
 import SoundProvider, { SoundContext } from '../../../Providers/SoundContext';
+import { useLocation } from 'react-router-dom';
 
 const Icon_Poketacek = () => {
   const { soundOn, setSoundOn } = useContext(SoundContext);
   let audioAhojVyber = new Audio(
     '../../assets/sounds/soundsOfPoketacek/ahojVyber.mp3',
   );
+  let audioCoVidis = new Audio(
+    '../../assets/sounds/soundsOfPoketacek/coVidisnaObrazkuPismenko.mp3',
+  );
+  let audioVyberHrat = new Audio(
+    '../../assets/sounds/soundsOfPoketacek/VyberSiHrat.mp3',
+  );
+  let audioCoKSobe = new Audio(
+    '../../assets/sounds/soundsOfPoketacek/CoKSObePatri.mp3',
+  );
+  let audioNaObrazek = new Audio(
+    '../../assets/sounds/soundsOfPoketacek/naObrazekPismenko.mp3',
+  );
+  const poketacekHome = location.pathname === '/';
+  const poketacekLearn = location.pathname === '/learn';
+  const poketacekPlay = location.pathname === '/play';
+  const poketacekPexeso = location.pathname === '/pexeso';
+  const poketacekDragDrop = location.pathname === '/dragdrop';
+
   return (
     <img
       id="icon_poketacek"
       src={icon_poketacek}
       alt="mascot Poketacek"
       onClick={() => {
-        if (soundOn) {
-          audioAhojVyber.play();
+        if (poketacekHome) {
+          if (soundOn) {
+            audioAhojVyber.play();
+          }
+        } else if (poketacekLearn) {
+          if (soundOn) {
+            audioCoVidis.play();
+          }
+        } else if (poketacekPlay) {
+          if (soundOn) {
+            audioVyberHrat.play();
+          }
+        } else if (poketacekPexeso) {
+          if (soundOn) {
+            audioCoKSobe.play();
+          }
+        } else if (poketacekDragDrop) {
+          if (soundOn) {
+            audioNaObrazek.play();
+          }
         }
       }}
     />
