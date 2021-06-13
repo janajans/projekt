a02f0bdde30999136545c91b709de5348aaf3507
janajans <1janajansova@gmail.com>
2021-06-13 14:56:34 +0200

    audio added to WinningPage component!

--- src/Components/WinningPage/index.jsx
+++ src/Components/WinningPage/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { SoundContext } from '../../Providers/SoundContext';
 import poketacek_loo from './img/poketacek_loo.svg';
 import './style.css';
 
-const WinningPage = () => (
-  <object type="image/svg+xml" data={poketacek_loo} style={{ width: '200px' }}>
-    svg-animation
-  </object>
-);
+const WinningPage = () => {
+  const { soundOn } = useContext(SoundContext);
+
+  let audioApplause = new Audio('../../assets/sounds/applause.mp3');
+  if (soundOn) {
+    audioApplause.play();
+  }
+
+  return (
+    <object
+      type="image/svg+xml"
+      data={poketacek_loo}
+      style={{ width: '200px' }}
+    >
+      svg-animation
+    </object>
+  );
+};
 
 export default WinningPage;
