831a7b13086eb445136dba5452e179beef0e586f
Viktorie Melichová <viktoriemelichova@gmail.com>
2021-06-13 21:19:01 +0200

    finished styling fo the header - for the n-th time

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,10 +3,10 @@ import './style.css';
 
 const Footer = () => (
   <footer>
-    <div className="footer-container">
+    <span className="footer-container">
       Vyrobeno jako projekt <a href="https://poketo.cz/">Poketo</a> a{' '}
       <a href="https://www.czechitas.cz/cs/">Czechitas</a>, 2021.
-    </div>
+    </span>
   </footer>
 );
 
